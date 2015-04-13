6eb466729eb25fbc3fb2fc0e794355ef4d3ea9b9
Usman Anwar <uanwar88@gmail.com>
2015-04-13 10:51:43 -0500

    Added regex to username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :password, presence: true, length: { minimum: 6 }
-  validates :username, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { within: 4..20 },
+    format: { with: /\A[a-zA-Z0-9_-]+\Z/ }
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /@/
 end
